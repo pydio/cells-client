9c30db9edbf12d4502ecbfe1295caaedf65aee0c
Bruno Sinou <bruno.sinou@posteo.de>
2019-09-19 17:22:47 +0200

    Add a few commands to deal with versions

--- cmd/version.go
+++ cmd/version.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	hashivers "github.com/hashicorp/go-version"
@@ -15,9 +16,17 @@ var (
 	version       string
 )
 
-// versionCmd represents the versioning command
-var versionCmd = &cobra.Command{
+var verCmd = &cobra.Command{
 	Use:   "version",
+	Short: "Version related commands",
+	Run: func(cm *cobra.Command, args []string) {
+		cm.Usage()
+	},
+}
+
+// showCmd displays information about the current version of this software.
+var showCmd = &cobra.Command{
+	Use:   "show",
 	Short: "Display current version of this software",
 	Run: func(cmd *cobra.Command, args []string) {
 
@@ -42,8 +51,63 @@ var versionCmd = &cobra.Command{
 	},
 }
 
+var ivCmd = &cobra.Command{
+	Use:   "isvalid",
+	Short: "Return an error if the passed version is not correctly formatted",
+	Long:  `Tries to parse passed version as string using the hashicorp/go-version library`,
+	Run: func(cm *cobra.Command, args []string) {
+		if len(args) != 1 {
+			cm.Printf("Please provide a version to parse\n")
+			os.Exit(1)
+		}
+
+		versionStr := args[0]
+		fmt.Printf("Checking version %s \n", versionStr)
+
+		_, err := hashivers.NewVersion(versionStr)
+		if err != nil {
+			cm.Printf("Passed version [%s] is not a valid version\n", versionStr)
+			os.Exit(1)
+		}
+	},
+}
+
+var igtCmd = &cobra.Command{
+	Use:   "isgreater",
+	Short: "Compares the two passed versions and returns true if the first is strictly greater than the second",
+	Long: `Tries to parse passed versions as string using the hashicorp/go-version library and returns an error if:
+	- one of the 2 strings cannot be parsed
+	- the first version is not strictly greater than the second`,
+	Run: func(cm *cobra.Command, args []string) {
+		if len(args) != 2 {
+			cm.Printf("Please provide two versions to be compared\n")
+			os.Exit(1)
+		}
+
+		v1Str := args[0]
+		v2Str := args[1]
+		fmt.Printf("Comparing versions %s & %s \n", v1Str, v2Str)
+
+		v1, err := hashivers.NewVersion(v1Str)
+		if err != nil {
+			cm.Printf("Passed version [%s] is not a valid version\n", v1Str)
+			os.Exit(1)
+		}
+		v2, err := hashivers.NewVersion(v2Str)
+		if err != nil {
+			cm.Printf("Passed version [%s] is not a valid version\n", v2Str)
+			os.Exit(1)
+		}
+		if !v1.GreaterThan(v2) {
+			cm.Printf("Passed version [%s] is *not* greater than [%s]\n", v1Str, v2Str)
+			os.Exit(1)
+		}
+	},
+}
+
 func init() {
-
-	RootCmd.AddCommand(versionCmd)
-
+	verCmd.AddCommand(showCmd)
+	verCmd.AddCommand(ivCmd)
+	verCmd.AddCommand(igtCmd)
+	RootCmd.AddCommand(verCmd)
 }
