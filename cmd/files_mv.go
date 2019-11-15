448cece0174eecb211afed52cc18713b9d40e6d3
j <jihad.thabet@gmail.com>
2019-11-15 17:08:40 +0100

    Mv (move nodes) command

---
+++ cmd/files_mv.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"log"
+	"path"
+
+	"github.com/spf13/cobra"
+
+	"github.com/pydio/cells-client/rest"
+)
+
+const filesMvCmdExample = `
+# Move a node
+./cec mv common-files/formula-one.jpg personal-files/photos/
+
+# Rename a node
+./cec mv common-files/formula-one.jpg common-files/f1.jpg
+
+# Move all nodes recursively 
+./cec mv common-files/photos/* personal-files/photos/
+`
+
+// filesMvCmd represents the filesMv command
+var filesMvCmd = &cobra.Command{
+	Use:     "mv",
+	Short:   "Move, Rename nodes",
+	Example: filesMvCmdExample,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 2 {
+			cmd.Help()
+			log.Fatalln("Missing Source and Target path")
+		}
+		source := args[0]
+		target := args[1]
+
+		var sourceNodes []string
+		if path.Base(source) == "*" {
+			nodes, _ := rest.ListNodesPath(source)
+			sourceNodes = nodes
+		} else {
+			_, exists := rest.StatNode(source)
+			if !exists {
+				log.Fatalf("This node does not exist: [%v]\n", source)
+			}
+			sourceNodes = []string{source}
+		}
+
+		params := rest.MoveParams(sourceNodes, target)
+
+		//TODO refactor
+		jobName := "move"
+		jobID, err := rest.RunJob(jobName, params)
+		if err != nil {
+			log.Fatalln("could not run job")
+		}
+
+		err = rest.MonitorJob(jobID)
+		if err != nil {
+			log.Fatalln("could not monitor job")
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(filesMvCmd)
+}
