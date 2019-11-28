c75edb538c224a3274a3301343606a016961692b
j <jihad.thabet@gmail.com>
2019-11-28 16:58:56 +0100

    test for path function

---
+++ tests/path_test.go
@@ -0,0 +1,54 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	. "github.com/pydio/cells-client/rest"
+)
+
+type testingPaths struct {
+	downloadTo       string
+	downloadFrom     string
+	downloadNodeFrom string
+	expectedResult   string
+}
+
+//Download Cases
+func TestSimpleTargetLocationPath(t *testing.T) {
+
+	mock := &testingPaths{
+		downloadTo:       "/Users/j/lulu",
+		downloadFrom:     "personal-files/formula-one",
+		downloadNodeFrom: "personal-files/formula-one/cars/redbull/verstappen.jpg",
+		expectedResult:   "/Users/j/lulu/formula-one/cars/redbull/verstappen.jpg",
+	}
+
+	result := TargetLocation(mock.downloadTo, mock.downloadFrom, mock.downloadNodeFrom)
+	assert.Equal(t, mock.expectedResult, result)
+}
+
+func TestComplicatedTargetLocationPath(t *testing.T) {
+
+	mock := &testingPaths{
+		downloadTo:       "/Users/j/lulu",
+		downloadFrom:     "personal-files/formula-one/2019 season",
+		downloadNodeFrom: "personal-files/formula-one/2019 season/tracks/monza/ferrari/vettel/first-corner.jpg",
+		expectedResult:   "/Users/j/lulu/2019 season/tracks/monza/ferrari/vettel/first-corner.jpg",
+	}
+	result := TargetLocation(mock.downloadTo, mock.downloadFrom, mock.downloadNodeFrom)
+	assert.Equal(t, mock.expectedResult, result)
+}
+
+func TestComplicatedSourceLocationPath(t *testing.T) {
+
+	mock := &testingPaths{
+		downloadTo:       "/Users/j/Downloads/my server/cells-test.your-files-your-rules.eu/",
+		downloadFrom:     "personal-files/formula-one/2019 season",
+		downloadNodeFrom: "personal-files/formula-one/2019 season/tracks/monza/ferrari/leclerc/first-corner.jpg",
+		expectedResult:   "/Users/j/Downloads/my server/cells-test.your-files-your-rules.eu/2019 season/tracks/monza/leclerc/first-corner.jpg",
+	}
+	result := TargetLocation(mock.downloadTo, mock.downloadFrom, mock.downloadNodeFrom)
+	assert.Equal(t, mock.expectedResult, result)
+}
