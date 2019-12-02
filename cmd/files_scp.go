2852c7be2347b04d274774b645bb810108b0fbea
cdujeu <charles@ajaxplorer.info>
2019-12-02 12:36:33 +0100

    Refactor scp

--- cmd/files_scp.go
+++ cmd/files_scp.go
@@ -2,20 +2,12 @@ package cmd
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
-	"sync"
-	"time"
-
-	"github.com/gosuri/uiprogress"
 
 	"github.com/spf13/cobra"
-
-	"github.com/pydio/cells-sdk-go/models"
 
 	. "github.com/pydio/cells-client/rest"
 )
@@ -43,8 +35,7 @@ const (
 )
 
 var (
-	currentPrefix          string
-	sourcePath, targetPath string
+	currentPrefix string
 )
 
 var scpFiles = &cobra.Command{
@@ -68,55 +59,24 @@ var scpFiles = &cobra.Command{
 			currentPrefix = prefixB
 		}
 
-		fmt.Printf("Copying %s to %s\n", from, to)
+		DryRun = false // Debug option
+		var crawlerPath, targetPath string
+		var isLocal bool
 
-		if strings.HasPrefix(from, prefixA) || strings.HasPrefix(from, prefixB) {
+		if strings.HasPrefix(from, currentPrefix) {
 			// Download
 			fromPath := strings.TrimPrefix(from, currentPrefix)
-			_, remote, e := targetToFullPath(to, from)
-
+			localTarget, isRemote, e := targetToFullPath(to, from)
 			if e != nil {
 				log.Fatal(e)
 			}
-			if remote {
+			if isRemote {
 				log.Fatal(fmt.Errorf("source and target are both remote, copy remote to local or the opposite"))
 			}
-
-			//TODO targetToFullPath already creates the targetedLocation path -- just append the node
-			//TODO targetToFullPath handles path such as "."
-			// Use it
-			sourcePath = fromPath
-			to, _ := filepath.Abs(to)
-			targetPath = to
-			// dl rec
-			if err := downloadRecursive(fromPath, to); err != nil {
-				log.Fatal(err)
-			}
-
-			log.Println("Download finished !")
-			//reader, length, e := rest.GetFile(fromPath)
-			//if e != nil {
-			//	log.Fatal(e)
-			//}
-			//bar := uiprogress.AddBar(length).PrependCompleted()
-			//wrapper := &PgReader{
-			//	Reader: reader,
-			//	bar:    bar,
-			//	total:  length,
-			//}
-			//writer, e := os.OpenFile(toPath, os.O_CREATE|os.O_WRONLY, 0755)
-			//if e != nil {
-			//	log.Fatal(e)
-			//}
-			//defer writer.Close()
-			//uiprogress.Start()
-			//_, e = io.Copy(writer, wrapper)
-			//if e != nil {
-			//	log.Fatal(e)
-			//}
-			//// Wait that progress bar finish rendering
-			//<-time.After(100 * time.Millisecond)
-			//uiprogress.Stop()
+			crawlerPath = fromPath
+			isLocal = false
+			targetPath = localTarget
+			fmt.Printf("Downloading %s to %s\n", from, to)
 
 		} else {
 			// Upload
@@ -127,59 +87,36 @@ var scpFiles = &cobra.Command{
 			if !remote {
 				log.Fatal(fmt.Errorf("source and target are both local, copy remote to local or the opposite"))
 			}
+			crawlerPath = from
+			isLocal = true
+			targetPath = strings.TrimPrefix(to, currentPrefix)
+			fmt.Printf("Uploading %s to %s\n", from, to)
+		}
 
-			//TODO targetToFullPath already creates the targetedLocation path -- just append the node
-			//TODO targetToFullPath handles path such as "."
-			// Use it
+		crawler, e := NewCrawler(crawlerPath, isLocal)
+		if e != nil {
+			log.Fatal(e)
+		}
+		nn, e := crawler.Walk()
+		if e != nil {
+			log.Fatal(e)
+		}
+		targetNode := NewTarget(targetPath, crawler)
 
-			// upload
-			to = strings.TrimPrefix(to, currentPrefix)
-			sourcePath = from
-			targetPath = to
-			from, _ = filepath.Abs(from)
-			if err := UploadRecursive(from, to); err != nil {
-				log.Fatal(err)
-			}
+		pool := NewBarsPool(len(nn) > 1, len(nn))
+		pool.Start()
 
-			//var length int64
-			//if s, e := os.Stat(from); e != nil || s.IsDir() {
-			//	log.Fatal(fmt.Errorf("local source is not a valid file"))
-			//} else {
-			//	length = s.Size()
-			//}
-			//reader, e := os.Open(from)
-			//bar := uiprogress.AddBar(int(length)).PrependCompleted()
-			//wrapper := &PgReader{
-			//	Reader: reader,
-			//	Seeker: reader,
-			//	bar:    bar,
-			//	total:  int(length),
-			//	double: true,
-			//}
-			//if e != nil {
-			//	log.Fatal(e)
-			//}
-			//uiprogress.Start()
-			//_, e = rest.PutFile(toPath, wrapper, false)
-			//if e != nil {
-			//	log.Fatal(e)
-			//}
-			//<-time.After(500 * time.Millisecond)
-			//uiprogress.Stop()
-			//// Now stat Node to make sure it is indexed
-			//e = rest.RetryCallback(func() error {
-			//	fmt.Println(" ## Waiting for file to be indexed...")
-			//	_, ok := rest.StatNode(toPath)
-			//	if !ok {
-			//		return fmt.Errorf("cannot stat node just after PutFile operation")
-			//	}
-			//	return nil
-			//
-			//}, 3, 3*time.Second)
-			//if e != nil {
-			//	log.Fatal("File does not seem to be indexed!")
-			//}
-			//fmt.Println(" ## File correctly indexed")
+		// CREATE FOLDERS
+		e = targetNode.MkdirAll(nn, pool)
+		if e != nil {
+			log.Fatal(e)
+		}
+
+		// UPLOAD / DOWNLOAD FILES
+		errs := targetNode.CopyAll(nn, pool)
+		//pool.Stop()
+		if len(errs) > 0 {
+			log.Fatal(errs)
 		}
 	},
 }
@@ -190,293 +127,29 @@ func init() {
 
 func targetToFullPath(to, from string) (string, bool, error) {
 	var toPath string
-	var isDir bool
+	//var isDir bool
 	var isRemote bool
 	var e error
 	if strings.HasPrefix(to, currentPrefix) {
 		// This is remote
 		isRemote = true
 		toPath = strings.TrimPrefix(to, currentPrefix)
-		target, ok := StatNode(toPath)
+		_, ok := StatNode(toPath)
 		if !ok {
 			// Does not exists => will be created
 			return toPath, isRemote, nil
 		}
-		isDir = target.Type == models.TreeNodeTypeCOLLECTION
 	} else {
 		// This is local
 		toPath, e = filepath.Abs(to)
 		if e != nil {
 			return "", false, e
 		}
-		s, e := os.Stat(toPath)
+		_, e := os.Stat(toPath)
 		if e != nil {
 			return "", false, e
 		}
-		isDir = s.IsDir()
 	}
 
-	if isDir {
-		toPath = path.Join(toPath, path.Base(from))
-	}
 	return toPath, isRemote, nil
-}
-
-type PgReader struct {
-	io.Reader
-	io.Seeker
-	bar   *uiprogress.Bar
-	total int
-	read  int
-
-	double bool
-	first  bool
-}
-
-func (r *PgReader) Read(p []byte) (n int, err error) {
-	n, err = r.Reader.Read(p)
-	if err == nil {
-		if r.double {
-			r.read += n / 2
-		} else {
-			r.read += n
-		}
-		r.bar.Set(r.read)
-	} else if err == io.EOF {
-		if r.double && !r.first {
-			r.first = true
-			r.bar.Set(r.total / 2)
-		} else {
-			r.bar.Set(r.total)
-		}
-	}
-	return
-}
-
-func (r *PgReader) Seek(offset int64, whence int) (int64, error) {
-	if r.double && r.first {
-		r.read = r.total/2 + int(offset)/2
-	} else {
-		r.read = int(offset)
-	}
-	r.bar.Set(r.read)
-	return r.Seeker.Seek(offset, whence)
-}
-
-func downloadRecursive(downloadFrom, downloadTo string) error {
-	nodes, err := walkRemote(downloadFrom, downloadTo, true)
-	if err != nil {
-		log.Fatalln("", err)
-	}
-
-	wg := &sync.WaitGroup{}
-	buf := make(chan struct{}, 3)
-
-	for _, n := range nodes {
-		if n.Type == models.TreeNodeTypeCOLLECTION {
-			continue
-		}
-		buf <- struct{}{}
-		wg.Add(1)
-		go func(nodePath string) {
-			defer func() {
-				<-buf
-				wg.Done()
-				//for bar.Incr() {
-				//	<-time.After(500 * time.Millisecond)
-				//}
-			}()
-
-			reader, _, e := GetFile(nodePath)
-			if e != nil {
-				log.Println("could not GetFile ", e)
-			}
-			//bar = uiprogress.NewBar(length)
-			//wrapper := &PgReader{
-			//	Reader: reader,
-			//	bar:    bar,
-			//	total:  length,
-			//}
-			downloadToLocation := TargetLocation(downloadTo, downloadFrom, nodePath)
-			writer, e := os.OpenFile(downloadToLocation, os.O_CREATE|os.O_WRONLY, 0755)
-			if e != nil {
-				log.Println("could not OpenFile ", e)
-			}
-			defer writer.Close()
-			_, e = io.Copy(writer, reader)
-			if e != nil {
-				log.Println("could not Copy", e)
-			}
-
-		}(n.Path)
-	}
-	wg.Wait()
-	//uiprogress.Stop()
-	return nil
-}
-
-func UploadRecursive(from, to string) error {
-	wg := &sync.WaitGroup{}
-	buf := make(chan struct{}, 3)
-	ll, err := walkLocal(from, to, true)
-	if err != nil {
-		return err
-	}
-	uiprogress.Start()
-	for _, l := range ll {
-		buf <- struct{}{}
-		wg.Add(1)
-
-		go func(uploadFrom, uploadTo string) {
-			defer func() {
-				<-buf
-				wg.Done()
-			}()
-			Upload(uploadFrom, uploadTo)
-		}(l.localNodePath, l.remoteNodePath)
-		wg.Wait()
-
-	}
-	uiprogress.Stop()
-	return nil
-}
-
-// upload take a local resource and puts it in the remote location
-func Upload(source string, target string) {
-	reader, e := os.Open(source)
-	if e != nil {
-		return
-	}
-	stats, _ := reader.Stat()
-	bar := uiprogress.AddBar(int(stats.Size())).PrependElapsed()
-	//bar.PrependFunc(func(b *uiprogress.Bar) string {
-	//	return "file: " + path.Base(target)
-	//})
-	wrapper := &PgReader{
-		Reader: reader,
-		Seeker: reader,
-		bar:    bar,
-		total:  int(stats.Size()),
-		double: true,
-	}
-
-	_, e = PutFile(target, wrapper, false)
-	if e != nil {
-		return
-	}
-	//FIXME disabled (will be re enabled later)
-	//// Now stat Node target make sure it is indexed
-	//e = RetryCallback(func() error {
-	//	//fmt.Println(" ## Waiting for file target be indexed...")
-	//	_, ok := StatNode(target)
-	//	if !ok {
-	//		return fmt.Errorf("cannot stat node just after PutFile operation")
-	//	}
-	//	return nil
-	//}, 3, 3*time.Second)
-	//if e != nil {
-	//	log.Fatal("File does not seem target be indexed!")
-	//}
-	for bar.Incr() {
-		<-time.After(500 * time.Millisecond)
-	}
-	fmt.Println(" ## File correctly indexed")
-}
-
-// TODO keep that?
-type localTree struct {
-	localNodePath  string
-	remoteNodePath string
-	info           os.FileInfo
-}
-
-// walkLocal walks the localTree and returns a struct with the localNode-path and the remoteNode-path to ease the Upload
-func walkLocal(fromLocal, toRemote string, createRemote bool) ([]localTree, error) {
-
-	var l localTree
-	var ll []localTree
-	var remoteDirPath []string
-
-	err := filepath.Walk(fromLocal, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if strings.HasPrefix(filepath.Base(path), ".") {
-			return nil
-		}
-		if info.IsDir() {
-			//TODO modify the dirpath toRemote have <target-path>/<source-folder>/...
-			remoteUploadPath := TargetLocation(toRemote, fromLocal, path)
-			remoteDirPath = append(remoteDirPath, remoteUploadPath)
-		} else {
-			l.remoteNodePath = TargetLocation(toRemote, fromLocal, path)
-			l.localNodePath = path
-			l.info = info
-			ll = append(ll, l)
-		}
-		return nil
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	if createRemote {
-		var nodes []*models.TreeNode
-		for _, dirP := range remoteDirPath {
-			nodes = append(nodes, &models.TreeNode{Path: dirP, Type: models.TreeNodeTypeCOLLECTION})
-		}
-		err = TreeCreateNodes(nodes)
-		if err != nil {
-			return nil, err
-		}
-		//FIXME make sure toRemote index after the creation - maybe add the index inside the tree function
-		//TODO stat node or something toRemote make sure that the nodes are there
-		//rest.RunJob("datasource-resync", "{\"dsName\":\"pydiods1\"}")
-	}
-	return ll, nil
-}
-
-// walkRemote lists all the nodes and if createLocal is set will create the tree on local system
-func walkRemote(fromRemote, toLocal string, createLocal ...bool) (nodes []*models.TreeNode, err error) {
-	var localTarget string
-	// lists nodes fromRemote server
-	nn, e := GetBulkMetaNode(fromRemote)
-	if e != nil {
-		err = e
-		return
-	}
-
-	for _, n := range nn {
-		nodes = append(nodes, n)
-		if n.Type == models.TreeNodeTypeCOLLECTION {
-			if len(createLocal) > 0 && createLocal[0] {
-				//Trim the star toLocal avoid errors during local path construction
-
-				fromRemote = strings.Trim(fromRemote, "*")
-				localTarget = TargetLocation(targetPath, sourcePath, n.Path)
-
-				if err = os.MkdirAll(localTarget, 0755); err != nil {
-					return
-				}
-			}
-			if children, e := walkRemote(path.Join(n.Path, "*"), toLocal, createLocal...); e != nil {
-				err = e
-				return
-			} else {
-				nodes = append(nodes, children...)
-			}
-		}
-	}
-	return
-}
-
-// tbd: download -> /Users/j/downloads/ + /personal-files/folder + /personal-files/folder/meteo.jpg = /Users/j/downloads/folder/meteo.jpg
-func TargetLocation(target, source, nodeSource string) string {
-
-	source = strings.Trim(source, "/")
-	nodeSource = strings.Trim(nodeSource, "/")
-	serverBase := path.Base(source)
-	relativePath := strings.TrimPrefix(nodeSource, source)
-
-	return filepath.Join(target, serverBase, relativePath)
 }
---
+++ rest/walker.go
@@ -0,0 +1,379 @@
+package rest
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gosuri/uiprogress"
+
+	"github.com/pydio/cells-sdk-go/models"
+)
+
+var (
+	DryRun    bool
+	QueueSize = 3
+)
+
+type CrawlNode struct {
+	IsLocal bool
+
+	IsDir    bool
+	FullPath string
+	RelPath  string
+	MTime    time.Time
+	Size     int64
+
+	os.FileInfo
+	models.TreeNode
+}
+
+func NewCrawler(target string, isLocal bool) (*CrawlNode, error) {
+	if isLocal {
+		i, e := os.Stat(target)
+		if e != nil {
+			return nil, e
+		}
+		return NewLocalNode(target, i), nil
+	} else {
+		n, b := StatNode(target)
+		if !b {
+			return nil, fmt.Errorf("not.found")
+		}
+		return NewRemoteNode(n), nil
+	}
+}
+
+func NewTarget(target string, source *CrawlNode) *CrawlNode {
+	c := &CrawlNode{
+		IsLocal:  !source.IsLocal,
+		IsDir:    source.IsDir,
+		FullPath: target,
+		RelPath:  "",
+	}
+	// For dirs, add source directory name
+	if source.IsDir {
+		c.FullPath = c.Join(c.FullPath, source.Base())
+	}
+	return c
+}
+
+func NewRemoteNode(t *models.TreeNode) *CrawlNode {
+	n := &CrawlNode{
+		IsDir:    t.Type == models.TreeNodeTypeCOLLECTION,
+		FullPath: strings.Trim(t.Path, "/"),
+	}
+	n.Size, _ = strconv.ParseInt(t.Size, 10, 64)
+	unixTime, _ := strconv.ParseInt(t.MTime, 10, 32)
+	n.MTime = time.Unix(unixTime, 0)
+	n.TreeNode = *t
+	return n
+}
+
+func NewLocalNode(fullPath string, i os.FileInfo) *CrawlNode {
+	fullPath = strings.TrimRight(fullPath, string(os.PathSeparator))
+	n := &CrawlNode{
+		IsLocal:  true,
+		IsDir:    i.IsDir(),
+		FullPath: fullPath,
+		MTime:    i.ModTime(),
+		Size:     i.Size(),
+	}
+	n.FileInfo = i
+	return n
+}
+
+func (c *CrawlNode) MkdirAll(dd []*CrawlNode, pool *BarsPool) error {
+	// Remote : prepare TreeNodes list and append root if required
+	var mm []*models.TreeNode
+	if !c.IsLocal {
+		if _, b := StatNode(c.FullPath); !b {
+			mm = append(mm, &models.TreeNode{Path: c.FullPath, Type: models.TreeNodeTypeCOLLECTION})
+		}
+	} else {
+		if _, e := os.Stat(c.FullPath); e != nil {
+			if DryRun {
+				fmt.Println("MkDir: \t", c.FullPath)
+			} else if e1 := os.MkdirAll(c.FullPath, 0755); e1 != nil {
+				return e1
+			}
+		}
+	}
+	for _, d := range dd {
+		if !d.IsDir {
+			continue
+		}
+		newFolder := c.Join(c.FullPath, d.RelPath)
+		if DryRun {
+			fmt.Println("MkDir: \t", newFolder)
+			continue
+		}
+		if c.IsLocal {
+			if e := os.MkdirAll(newFolder, 0755); e != nil {
+				return e
+			} else {
+				pool.Done()
+			}
+		} else {
+			mm = append(mm, &models.TreeNode{Path: newFolder, Type: models.TreeNodeTypeCOLLECTION})
+		}
+	}
+	if !c.IsLocal && !DryRun {
+		e := TreeCreateNodes(mm)
+		if e != nil {
+			return e
+		}
+		for range mm {
+			pool.Done()
+		}
+		// TODO:  Stat all folders to make sure they are indexed ?
+	}
+	return nil
+}
+
+func (c *CrawlNode) Walk(current ...string) (children []*CrawlNode, e error) {
+	crt := ""
+	if len(current) > 0 {
+		crt = current[0]
+	}
+	if !c.IsDir {
+		c.RelPath = c.Base()
+		children = append(children, c)
+		return
+	}
+	if c.IsLocal {
+		e = filepath.Walk(filepath.Join(c.FullPath, crt), func(p string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if strings.HasPrefix(filepath.Base(p), ".") {
+				return nil
+			}
+			n := NewLocalNode(p, info)
+			n.RelPath = strings.TrimPrefix(n.FullPath, c.FullPath)
+			children = append(children, n)
+			return nil
+		})
+	} else {
+		nn, er := GetBulkMetaNode(path.Join(c.FullPath, crt, "*"))
+		if er != nil {
+			e = er
+			return
+		}
+		for _, n := range nn {
+			remote := NewRemoteNode(n)
+			remote.RelPath = strings.TrimPrefix(remote.FullPath, c.FullPath)
+			children = append(children, remote)
+			if n.Type == models.TreeNodeTypeCOLLECTION {
+				cc, er := c.Walk(remote.RelPath)
+				if er != nil {
+					e = er
+					return
+				}
+				children = append(children, cc...)
+			}
+		}
+	}
+	return
+}
+
+func (c *CrawlNode) CopyAll(dd []*CrawlNode, pool *BarsPool) (errs []error) {
+	idx := -1
+	buf := make(chan struct{}, QueueSize)
+	wg := &sync.WaitGroup{}
+	for _, d := range dd {
+		if d.IsDir {
+			continue
+		}
+		buf <- struct{}{}
+		idx++
+		bar := pool.Get(idx, int(d.Size), d.Base())
+		wg.Add(1)
+		go func(src *CrawlNode, barId int) {
+			defer func() {
+				wg.Done()
+				pool.Done()
+				<-buf
+			}()
+			if !c.IsLocal {
+				if e := c.upload(src, bar); e != nil {
+					errs = append(errs, e)
+				}
+			} else {
+				if e := c.download(src, bar); e != nil {
+					errs = append(errs, e)
+				}
+			}
+		}(d, idx)
+	}
+	wg.Wait()
+	pool.Stop()
+	return
+}
+
+func (c *CrawlNode) upload(src *CrawlNode, bar *uiprogress.Bar) error {
+	reader, e := os.Open(src.FullPath)
+	if e != nil {
+		return e
+	}
+	stats, _ := reader.Stat()
+	wrapper := &PgReader{
+		Reader: reader,
+		Seeker: reader,
+		bar:    bar,
+		total:  int(stats.Size()),
+		double: true,
+	}
+	_, e = PutFile(c.Join(c.FullPath, src.RelPath), wrapper, false)
+	return e
+}
+
+func (c *CrawlNode) download(src *CrawlNode, bar *uiprogress.Bar) error {
+	reader, length, e := GetFile(src.FullPath)
+	if e != nil {
+		return e
+	}
+	wrapper := &PgReader{
+		Reader: reader,
+		bar:    bar,
+		total:  length,
+	}
+	downloadToLocation := c.Join(c.FullPath, src.RelPath)
+	writer, e := os.OpenFile(downloadToLocation, os.O_CREATE|os.O_WRONLY, 0755)
+	if e != nil {
+		return e
+	}
+	defer writer.Close()
+	_, e = io.Copy(writer, wrapper)
+	return e
+}
+
+func (c *CrawlNode) Join(p ...string) string {
+	if os.PathSeparator != '/' {
+		for i, pa := range p {
+			if c.IsLocal {
+				p[i] = strings.ReplaceAll(pa, "/", string(os.PathSeparator))
+			} else {
+				p[i] = strings.ReplaceAll(pa, string(os.PathSeparator), "/")
+			}
+		}
+	}
+	if c.IsLocal {
+		return filepath.Join(p...)
+	} else {
+		return path.Join(p...)
+	}
+}
+
+func (c *CrawlNode) Base() string {
+	if c.IsLocal {
+		return filepath.Base(c.FullPath)
+	} else {
+		return path.Base(c.FullPath)
+	}
+}
+
+type BarsPool struct {
+	*uiprogress.Progress
+	showGlobal bool
+	nodesBar   *uiprogress.Bar
+}
+
+func NewBarsPool(showGlobal bool, totalNodes int) *BarsPool {
+	b := &BarsPool{}
+	b.Progress = uiprogress.New()
+	b.showGlobal = showGlobal
+	if showGlobal {
+		b.nodesBar = b.AddBar(totalNodes)
+		b.nodesBar.PrependCompleted()
+		b.nodesBar.AppendFunc(func(b *uiprogress.Bar) string {
+			if b.Current() == b.Total {
+				return fmt.Sprintf("Transferred %d/%d files and folders (%s)", b.Current(), b.Total, b.TimeElapsedString())
+			} else {
+				return fmt.Sprintf("Transfering %d/%d files or folders", b.Current()+1, b.Total)
+			}
+		})
+	}
+	return b
+}
+
+func (b *BarsPool) Done() {
+	if !b.showGlobal {
+		return
+	}
+	b.nodesBar.Incr()
+	if b.nodesBar.Current() == b.nodesBar.Total {
+		// Finished, remove all bars
+		b.Bars = []*uiprogress.Bar{b.nodesBar}
+	}
+}
+
+func (b *BarsPool) Get(i int, total int, name string) *uiprogress.Bar {
+	idx := i % QueueSize
+	var nBars []*uiprogress.Bar
+	if b.showGlobal {
+		idx++
+		nBars = append(nBars, b.nodesBar)
+	}
+	// Remove old bar
+	for k, bar := range b.Bars {
+		if k == idx || (b.showGlobal && bar == b.nodesBar) {
+			continue
+		}
+		nBars = append(nBars, bar)
+	}
+	b.Bars = nBars
+	bar := b.AddBar(total)
+	bar.PrependCompleted()
+	bar.AppendFunc(func(b *uiprogress.Bar) string {
+		return fmt.Sprintf("%s", name)
+	})
+	return bar
+}
+
+type PgReader struct {
+	io.Reader
+	io.Seeker
+	bar   *uiprogress.Bar
+	total int
+	read  int
+
+	double bool
+	first  bool
+}
+
+func (r *PgReader) Read(p []byte) (n int, err error) {
+	n, err = r.Reader.Read(p)
+	if err == nil {
+		if r.double {
+			r.read += n / 2
+		} else {
+			r.read += n
+		}
+		r.bar.Set(r.read)
+	} else if err == io.EOF {
+		if r.double && !r.first {
+			r.first = true
+			r.bar.Set(r.total / 2)
+		} else {
+			r.bar.Set(r.total)
+		}
+	}
+	return
+}
+
+func (r *PgReader) Seek(offset int64, whence int) (int64, error) {
+	if r.double && r.first {
+		r.read = r.total/2 + int(offset)/2
+	} else {
+		r.read = int(offset)
+	}
+	r.bar.Set(r.read)
+	return r.Seeker.Seek(offset, whence)
+}
