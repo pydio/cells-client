8c545eabe5acb7c798d16d01ff8b04b38360d0a0
Bruno Sinou <bruno.sinou@posteo.de>
2019-12-03 17:52:25 +0100

    Better root comment

--- cmd/root.go
+++ cmd/root.go
@@ -12,12 +12,39 @@ import (
 	"github.com/pydio/cells-client/rest"
 )
 
-var (
-	configFile string
-)
+var configFile string
 
-var (
-	bash_completion_func = `__` + os.Args[0] + `_custom_func() {
+// RootCmd is the parent of all commands defined in this package.
+// It takes care of the pre-configuration of the defaut connection to the SDK in its PersistentPreRun phase.
+var RootCmd = &cobra.Command{
+	Use:                    os.Args[0],
+	Short:                  "Connect to a Pydio Cells server using the command line",
+	BashCompletionFunction: bash_completion_func,
+	Long: `
+# This tool uses the REST API to connect to your Pydio Cells server instance
+
+Pydio Cells comes with a powerful REST API that exposes various endpoints and enables management of the running instance.
+As a convenience, the Pydio team also provides a ready to use SDK for the Go language that encapsulates the boiling plate code to wire things 
+and provides a few chosen utilitary methods to ease implementation when using the SDK in various Go programs.
+
+See the help of the various commands for detailed explanation and some examples. 
+`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+
+		if cmd.Use != "configure" && cmd.Use != "oauth" && cmd.Use != "clear" && cmd.Use != "doc" {
+			e := rest.SetUpEnvironment(configFile)
+			if e != nil {
+				log.Fatalf("cannot read config file, please make sure to run '%s oauth' first. (Error: %s)\n", os.Args[0], e.Error())
+			}
+		}
+
+  },
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
+}
+
+var bash_completion_func = `__` + os.Args[0] + `_custom_func() {
   case ${last_command} in
   ` + os.Args[0] + `_mv | ` + os.Args[0] + `_cp | ` + os.Args[0] + `_rm | ` + os.Args[0] + `_ls)
     _path_completion
@@ -94,42 +121,8 @@ _datasources_completion() {
   COMPREPLY=($(compgen -W "${dsopts[@]}" -- "$cur"))
 }
 `
-)
-
-// RootCmd is the parent of all example commands defined in this package.
-// It takes care of the pre-configuration of the defaut connection to the SDK
-// in its PersistentPreRun phase.
-var RootCmd = &cobra.Command{
-	Use:                    os.Args[0],
-	Short:                  "Connect to a Cells Server using the command line",
-	BashCompletionFunction: bash_completion_func,
-	Long: `
-# This tool uses the REST API to connect a Cells Server.
-
-Pydio Cells comes with a powerful REST API that exposes various endpoints and enable management of a running Cells instance.
-As a convenience, the Pydio team also provide a ready to use SDK for the Go language that encapsulates the boiling code to wire things 
-and provides a few chosen utilitary methods to ease implemantation when using the SDK in various Go programs.
-
-The children commands defined here show some basic examples of what can be achieved when combining the use of this SDK with 
-the powerful Cobra framework to easily implement small CLI client applications.
-`,
-	PersistentPreRun: func(cmd *cobra.Command, args []string) {
-
-		if cmd.Use != "configure" && cmd.Use != "oauth" && cmd.Use != "clear" && cmd.Use != "doc" {
-			e := rest.SetUpEnvironment(configFile)
-			if e != nil {
-				log.Fatalf("cannot read config file, please make sure to run %s configure first (error %s)\n", os.Args[0], e)
-			}
-		}
-
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		cmd.Help()
-	},
-}
 
 func init() {
 	flags := RootCmd.PersistentFlags()
 	flags.StringVarP(&configFile, "config", "c", "", "Path to the configuration file")
-
 }
