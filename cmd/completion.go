7d2da8bccf1441cf793285f658ce6e9373a1eff9
j <jihad.thabet@gmail.com>
2019-12-11 16:59:55 +0100

    Cosmetic changes (characters in description are messing up with the markdown)

--- cmd/completion.go
+++ cmd/completion.go
@@ -19,39 +19,39 @@ This command installs an additional plugin to provide suggestions when working w
 
 1) Using Bash
 
-## On Linux, you must insure the 'bash-completion' library is installed:
-# on Debian / Ubuntu
-sudo apt install bash-completion
+	On Linux, you must insure the 'bash-completion' library is installed:
+		Debian / Ubuntu
+			sudo apt install bash-completion
+		
+		RHEL / CentOS
+			sudo yum install bash-completion
+		
+		On MacOS
+			brew install bash-completion
+			(make sure to follow the instructions displayed on Homebrew)
 
-# on RHEL / CentOS
-sudo yum install bash-completion
-
-# on MacOS
-brew install bash-completion
-(make sure to follow the instructions displayed on Homebrew)
-
-# Then, to enable completion in your current session:
+Then, to enable completion in your current session:
 	
 	source <(` + os.Args[0] + ` completion bash)
 
-# Or in a persistent manner:
+Or in a persistent manner:
 
-## Debian/Ubuntu/CentOS
-	` + os.Args[0] + ` completion bash | sudo tee /etc/bash_completion.d/cec
+	Debian/Ubuntu/CentOS
+		` + os.Args[0] + ` completion bash | sudo tee /etc/bash_completion.d/cec
 
-## macOS
-	` + os.Args[0] + ` completion bash | tee /usr/local/etc/bash_completion.d/cec
+	MacOS
+		` + os.Args[0] + ` completion bash | tee /usr/local/etc/bash_completion.d/cec
 
 2) Using Zsh
 
-# Add to current zsh session:
-	source <(` + os.Args[0] + ` completion zsh)
+	Add to current zsh session:
+		source <(` + os.Args[0] + ` completion zsh)
 
-	# Add in a persistent manner:
-	` + os.Args[0] + ` completion zsh | sudo tee <path>/<to>/<your zsh completion folder>
+	Add in a persistent manner:
+		` + os.Args[0] + ` completion zsh | sudo tee <path>/<to>/<your zsh completion folder>
 	
-	# On macOS
-	` + os.Args[0] + ` completion zsh | sudo tee /Users/<your current user>/.zsh/completion/_cec
+	On macOS
+		` + os.Args[0] + ` completion zsh | tee /Users/<your current user>/.zsh/completion/_cec
 	 `,
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
