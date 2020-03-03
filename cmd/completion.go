21dce03f72cb43f11f4a044f6570231b1b14f8a9
jthabet <jihad.thabet@gmail.com>
2020-03-03 13:53:33 +0100

    Cosmetic change

--- cmd/completion.go
+++ cmd/completion.go
@@ -6,7 +6,38 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var shType string
+var completionCmdExample = `1) Using Bash
+
+	# Add to current zsh session:
+		source <(` + os.Args[0] + ` completion bash)
+
+	# Debian/Ubuntu/CentOS
+		` + os.Args[0] + ` completion bash | sudo tee /etc/bash_completion.d/cec
+
+	# MacOS
+		` + os.Args[0] + ` completion bash | tee /usr/local/etc/bash_completion.d/cec
+
+2) Zsh
+
+	# Add to current zsh session:
+		source <(` + os.Args[0] + ` completion zsh)
+
+	# Debian/Ubuntu/CentOS:
+		` + os.Args[0] + ` completion zsh | sudo tee <path>/<to>/<your zsh completion folder>
+
+	# MacOS
+		` + os.Args[0] + ` completion zsh | tee /Users/<your current user>/.zsh/completion/_cec
+
+
+
+#### You must insure the 'bash-completion' library is installed:
+	# Debian / Ubuntu
+		sudo apt install bash-completion
+	# RHEL / CentOS
+		sudo yum install bash-completion
+	# On MacOS (be sure to follow the instructions displayed on Homebrew)
+		brew install bash-completion
+`
 
 var completionCmd = &cobra.Command{
 	Use:   "completion",
@@ -14,43 +45,9 @@ var completionCmd = &cobra.Command{
 	Long: `
 Install completion helper to Pydio Cells Client.
 
-This command installs an additional plugin to provide suggestions when working with the Cells Client and hitting the 'tab' key.
-
-1) Using Bash
-
-	On Linux, you must insure the 'bash-completion' library is installed:
-		Debian / Ubuntu
-			sudo apt install bash-completion
-		
-		RHEL / CentOS
-			sudo yum install bash-completion
-		
-		On MacOS (be sure to follow the instructions displayed on Homebrew)
-			brew install bash-completion
-
-Then, to enable completion in your current session:
-	
-	source <(` + os.Args[0] + ` completion bash)
-
-Or in a persistent manner:
-
-	Debian/Ubuntu/CentOS
-		` + os.Args[0] + ` completion bash | sudo tee /etc/bash_completion.d/cec
-
-	MacOS
-		` + os.Args[0] + ` completion bash | tee /usr/local/etc/bash_completion.d/cec
-
-2) Using Zsh
-
-	Add to current zsh session:
-		source <(` + os.Args[0] + ` completion zsh)
-
-	Add in a persistent manner:
-		` + os.Args[0] + ` completion zsh | sudo tee <path>/<to>/<your zsh completion folder>
-	
-	On macOS
-		` + os.Args[0] + ` completion zsh | tee /Users/<your current user>/.zsh/completion/_cec
-	 `,
+This command installs an additional plugin to provide suggestions when working with the Cells Client and hitting the 'tab' key.`,
+	Example: completionCmdExample,
+	Args:    cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
