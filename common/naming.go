705b05e0eadcb101ebc14ee62acbd90aaf105d96
jthabet <jihad.thabet@gmail.com>
2020-06-02 18:33:07 +0200

    Add pubkey

--- common/naming.go
+++ common/naming.go
@@ -10,7 +10,6 @@ var (
 const (
 	UpdatePackageType = "CellsClient"
 	UpdateChannel     = "dev"
-	// TODO DO NOT COMMIT THIS
-	UpdateServerUrl = ""
-	UpdatePublicKey = ""
+	UpdateServerUrl   = ""
+	UpdatePublicKey   = "-----BEGIN PUBLIC KEY-----\nMIIBCgKCAQEAqtnEiO46Mw5mkRpA+CC970pU7a8R3ZYhEoS+iAkex+s0MUWWkw53\n0s3eMs0chmsMAfVm/OUnlvvgZwxpypondjFT4prB1XQildessFNBR+mGfJP4krUr\nQz4sQE3525AxTBj5/ulpuwGgYdp5vvRre6eq4yr40adA8p8/yZFM9AVLiz65fVwp\ngyyLBYUBXtWsGLza9CeYsUK88WOG1J5U+SjnKUeL+kAib+5AtDZZ9Tn/BQve5aXS\n8XTFWCty72rqhousfGn1Ek78wQpbv0pihS3QlKXbZhmEFbdqpvPZmgI/ZzXn6mBR\nSJyNaQWiMj2VJjBDIgGxSx60b7/Tk4lLQwIDAQAB\n-----END PUBLIC KEY-----"
 )
