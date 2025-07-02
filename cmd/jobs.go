6dab12a7a532e92d122085c8081903a4b09aae96
Bruno Sinou <bruno.sinou@posteo.de>
2025-07-02 18:58:32 +0200

    chore: improve and standardize both inline doc and variable names

--- cmd/jobs.go
+++ cmd/jobs.go
@@ -5,16 +5,16 @@ import (
 )
 
 var jobsCmd = &cobra.Command{
-	Use: "jobs",
-	Short: "Jobs commands",
+	Use:   "jobs",
+	Short: "Manage existing Cells Flow Jobs",
 	Long: `
 DESCRIPTION
 
- Commands to manage jobs
- See the help of respective sub-commands for further details.
+  Commands to manage jobs, mainly search, list and delete.
+  See the help of respective sub-commands for further details.
 	`,
 	Args: cobra.MinimumNArgs(1),
-	Run: func(cm *cobra.Command, args []string){
+	Run: func(cm *cobra.Command, args []string) {
 		_ = cm.Usage()
 	},
 }
