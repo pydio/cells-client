92e302434cc2256fad99dbf8d26f20ac075e529f
Bruno Sinou <bruno.sinou@posteo.de>
2025-07-03 12:20:16 +0200

    chore: improve inline doc

--- cmd/jobs.go
+++ cmd/jobs.go
@@ -6,12 +6,26 @@ import (
 
 var jobsCmd = &cobra.Command{
 	Use:   "jobs",
-	Short: "Manage existing Cells Flow Jobs",
+	Short: "Manage jobs",
 	Long: `
 DESCRIPTION
 
-  Commands to manage jobs, mainly search, list and delete.
-  See the help of respective sub-commands for further details.
+  In Pydio Cells, a job represents the definition of a sequence of actions 
+  (basic building blocks that can be chained together) to process data and 
+  perform long running tasks in the background. 
+  Typically, copying a folder with many files in it.
+
+  There are 2 types of jobs:
+  - System Jobs: they are owned by the "pydio.system.user" user and can be triggered 
+    by events, the scheduler or manually launched. The server comes with a bunch of 
+    jobs that are useful to maintain your system and e.g perform house keeping tasks.
+    You can create new job templates using Cells Flow to fine-tune your business processes.
+  - User Jobs: they start upon events received when a user performs certain action 
+    (like extracting a thumbnail after successful upload of an image or deleting a large folder) 
+    and are then owned by the user who triggered the action.
+
+  Use the provided job sub-commands to search, list and delete existing jobs;
+  see their respective help for further details.
 	`,
 	Args: cobra.MinimumNArgs(1),
 	Run: func(cm *cobra.Command, args []string) {
